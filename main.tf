8ce2aed5ac12287e4ce2a3f2ad0acfc15bd64123
IG05 <ishaan2004goyal@gmail.com>
2025-06-09 22:16:21 +0530

    sixth

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
 resource "google_compute_address" "nginx_ip" {
   name   = "${var.vm_name}-ip"
   region = var.region
@@ -14,52 +20,6 @@ resource "google_compute_firewall" "allow_http" {
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
-}
-
-resource "google_compute_instance" "nginx_vm" {
-  name         = var.vm_name
-  machine_type = var.vm_machine_type
-  zone         = var.zone
-
-  tags = ["http-server"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-12"
-      size  = 20
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      nat_ip = google_compute_address.nginx_ip.address
-    }
-  }
-
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    apt-get update
-    apt-get install -y nginx
-
-    CLOUD_RUN_URL="https://${var.cloud_run_service_name}-${var.region}.a.run.app"
-
-    cat > /etc/nginx/sites-available/default <<EOF
-    server {
-      listen 80;
-      location / {
-        proxy_pass $CLOUD_RUN_URL;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-      }
-    }
-    EOF
-
-    systemctl restart nginx
-  EOT
 }
 
 resource "google_cloud_run_service" "demo_app" {
@@ -85,4 +45,61 @@ resource "google_cloud_run_service_iam_member" "public_invoker" {
   location = google_cloud_run_service.demo_app.location
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+resource "google_compute_instance" "nginx_vm" {
+  name         = var.vm_name
+  machine_type = var.vm_machine_type
+  zone         = var.zone
+
+  depends_on = [google_cloud_run_service.demo_app]
+
+  tags = ["http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 20
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.nginx_ip.address
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+
+    CLOUD_RUN_URL="${google_cloud_run_service.demo_app.status[0].url}"
+    CLOUD_RUN_HOST=$(echo $CLOUD_RUN_URL | awk -F/ '{print $3}')
+
+    cat > /etc/nginx/sites-available/default <<EOF
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+
+    location / {
+        proxy_pass $CLOUD_RUN_URL;
+
+        proxy_ssl_server_name on;
+        proxy_ssl_name $CLOUD_RUN_HOST;
+
+        proxy_ssl_verify off;
+
+        proxy_set_header Host $CLOUD_RUN_HOST;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOF
+
+    systemctl restart nginx
+  EOT
 }
