11ead8a1f0ffc8c48b241a55537bb16c3a2cce3b
IG05 <ishaan2004goyal@gmail.com>
2025-06-10 10:56:56 +0530

    eleventh

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
 
-        rewrite ^${var.path_prefix}/(.*)$ /\$1 break;
+        rewrite ^${var.path_prefix}(/.*)?$ \$1 break;
 
     }
 }
